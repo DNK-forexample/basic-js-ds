1ff7385905858738fd8fed93f360c04f443b8724
Dasha Konchyts <forexamplednk@gmail.com>
2022-10-18 02:20:35 +0300

    commit message

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class Queue {
 
   getUnderlyingList() {
- 
+    return this;
   }
 
-  enqueue(/* value */) {
-   
+  enqueue(value) {
+    if (!this.value) {
+      this.value = value;
+      return this.value;
+    }
+    let last = this;
+    while (last.next) {
+      last = last.next
+    }
+    last.next = new ListNode (value);
+    return this.value;
   }
 
   dequeue() {
-
+    let value = this.value;
+    this.value = this.next.value;
+    this.next = this.next.next;
+    return value;
   }
 }
-
 module.exports = {
   Queue
 };
